6f0eeb8f8b54edcf0f2775d3596e8bf80797535b
leozy <143453389+LeonardoBCL@users.noreply.github.com>
2023-12-14 18:45:50 -0300

    Update Questão01resolvida.c
    Corrigida

--- Questão01resolvida.c
+++ Questão01resolvida.c
@@ -1,77 +1,106 @@
-/*
-Crie uma struct Contato para representar informações de contatos, 
-incluindo nome, número de telefone e email.
-Solicite que o usuário cadastre 3 contatos. Escreva uma função que recebe um nome como parâmetro, 
-e retorna o número de telefone correspondente a esse nome. 
-Em seguida, implemente um programa que utiliza essa função para buscar e imprimir o número de telefone de um contato específico.
-*/
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <locale.h>
 
-struct ficha_contato
-{
-    char nome[200];
-    char telefone[200];
-    char email[200];
-};
+
+
+
+
+
+
+/*Crie uma struct Contato para representar informações de contatos, incluindo nome, número de telefone e email. 
+Solicite que o usuário cadastre 3 contatos. 
+Escreva uma função que recebe um nome como parâmetro, e retorna o número de telefone correspondente a esse nome.
+Em seguida, implemente um programa que utiliza essa função para buscar
+e imprimir o número de telefone de um contato específico.*/
 
 #define TAM 3
 
-char *buscar_telefone(struct ficha_contato contatos[], int numContatos, const char *nome)
-{
-    for (int contador = 0; contador < numContatos; contador++)
-    {
-        if (strcmp(contatos[contador].nome, nome) == 0)
-        {
-            return contatos[contador].telefone;
-        }
-    }
+struct contatos{
+	
+	char nome[200];
+	char numero_tel[200];
+	char email[200];
+	
+};
 
-}
 
-int main()
-{
-    setlocale(LC_ALL, "portuguese");
 
-    struct ficha_contato contatos[TAM];
-
-    printf("Cadastro de Contatos:\n");
-
-    for (int contador = 0; contador < TAM; contador++)
-    {
-        printf("Contato %d:\n", contador + 1);
-        printf("Nome: ");
-        scanf("%s", contatos[contador].nome);
-        printf("Telefone: ");
-        scanf("%s", contatos[contador].telefone);
-        printf("Email: ");
-        scanf("%s", contatos[contador].email);
-    }
-
-    char nome_busca[200];
-    printf("\nDigite o nome para buscar o telefone: ");
-    scanf("%s", nome_busca);
-
-    char *telefoneEncontrado;
-    telefoneEncontrado = buscar_telefone(contatos, TAM, nome_busca);
-
-    if (telefoneEncontrado != 0)
-    {
-        printf("Telefone encontrado: %s\n", telefoneEncontrado);
-    }
-    else
-    {
-        printf("Contato não encontrado.\n");
-    }
-
-    return 0;
+char *identificador (struct contatos agenda_de_contatos[], char *nome){
+/////////	
+	
+	int contador;
+	
+	for(contador = 0; contador < TAM; contador++){
+		
+	
+	if (strcmp(agenda_de_contatos[contador].nome, nome) == 0)
+	{
+		return agenda_de_contatos[contador].numero_tel;
+	}
+	
+	
+	}
+	
+//////////
 }
 
 
 
+
+int main(){
+	
+	//Selecionar a struct:
+	
+	struct contatos agenda_de_contatos[TAM];
+	
+	////////////////////////////////////////////
+	int contador;
+	
+	
+	for(contador = 1; contador < TAM; contador++){
+	
+	printf("--------Tela de cadastro---------\n");
+	
+	printf("Tela do %d cadastro: \n", contador);
+	printf("Por favor, registre o seu nome:\n ");
+	gets(agenda_de_contatos[contador].nome);
+    fflush(stdin);
+	printf("Por favor, registre o numero de telefone: \n ");
+	gets(agenda_de_contatos[contador].numero_tel);
+	
+    fflush(stdin);
+	printf("Por favor, insira o seu email: ");
+	gets(agenda_de_contatos[contador].email);
+	
+	
+	}
+	
+	
+	char buscar_nome[200];
+	printf("Digite o nome que desesja para buscar o telefone correspondente: ");
+	gets(buscar_nome);
+	
+	
+	char *telefone_encontrado;
+
+	telefone_encontrado = identificador(agenda_de_contatos, buscar_nome);
+	
+	if(telefone_encontrado != 0)
+	{
+		printf("Telefone encontrado: %s", telefone_encontrado);
+	}
+	else
+	{
+		printf("Contato não encontrado");
+	}
+	
+	return 0;
+	
+	
+	
+}
 
 
 
